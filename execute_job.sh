9d4f2700106ca538e960968142584e46b941cb6d
izzikampono <58926615+izzikampono@users.noreply.github.com>
2024-01-21 23:37:01 +0100

    execute_job.sh

---
+++ execute_job.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# This is a Bash script that dynamically sets Slurm directives and takes an additional argument
+
+# Check if at least two arguments are provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <job_name> <problem>"
+    exit 1
+fi
+
+# Access command-line arguments
+
+problem="$2"
+# Add more variables as needed
+
+# Slurm script filename
+slurm_script="job2.sh"
+
+# Create the Slurm script dynamically based on command-line arguments
+cat > "$slurm_script" <<EOL
+#!/bin/bash
+#SBATCH --job-name=python_cpu
+#SBATCH --output=$problem.csv
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=2G
+#SBATCH --error=error_file.txt
+#SBATCH --mem=8000
+#SBATCH --time=00:10:00
+
+echo "Problem: $problem"
+
+# Activate your Python environment 
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+source /scratch/s3918343/venvs/thesis/bin/activate
+
+cd /scratch/s3918343/venvs/thesis/Thesis
+
+# Your Slurm job commands go here
+python experiment.py problem="$problem" horizon=10 iter=10
+
+deactivate
+EOL
+
+# Make the Slurm script executable
+chmod +x "$slurm_script"
+
+# Submit the Slurm job
+sbatch "$slurm_script" "$2"
+
+# Exit the script
+exit 0
