8247a2fab60da135103562ba960b5ff7d831051c
izzikampono <58926615+izzikampono@users.noreply.github.com>
2024-01-22 13:08:47 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -13,14 +13,14 @@ module load Python/3.9.6-GCCcore-11.2.0
 
 
 # Check if at least one argument is provided
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
     echo "Usage: $0 <problem> [<horizon>] [num_iter]"
     exit 1
 fi
 
 source /scratch/s3918343/venvs/thesis/bin/activate
 pip install -r requirements.txt
-echo "Argument 1: $1"
+echo "problem : $1 , horizon: $2, iter : $3"
 
 
 cd /scratch/s3918343/venvs/thesis/Thesis
