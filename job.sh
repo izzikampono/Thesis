666c6145a418c3d023438cdc5b8c42ffe5ed7089
izzikampono <58926615+izzikampono@users.noreply.github.com>
2024-01-23 23:38:44 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=00:10:00
+#SBATCH --time=00:15:00
 #SBATCH --error=error_file_jobsh.txt
 #SBATCH --job-name=$1
 #SBATCH --mem=5G
