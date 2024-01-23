d9f165b92ea3fcd981abce9293709ddaa3d4e779
izzikampono <58926615+izzikampono@users.noreply.github.com>
2024-01-23 14:49:13 +0100

    Update experiment_server.py

--- experiment_server.py
+++ experiment_server.py
@@ -47,9 +47,8 @@ def initialize_database():
                     "number_of_beliefs" : [],
                     "leader_value_b0":[],
                     "follower_value_b0":[],
-                    "density" = [],
-                    "gap":[]
-                   
+                    "density" : [],
+                    "gap":[],
                     }
     return database
 
